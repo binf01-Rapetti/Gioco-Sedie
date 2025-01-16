86624695af4db0441317cd5f062a05648971695b
binf01-Rapetti <105106982+binf01-Rapetti@users.noreply.github.com>
2025-01-16 13:27:21 +0100

    Update Posto.java

--- Posto.java
+++ Posto.java
@@ -1,24 +1,29 @@
-package giocosedie;
+package GiocoSedieRapetti;
 
-class Posto
+class Posto {
+    private boolean occupato;
 
-{
-	private boolean occupato;
+    // Costruttore che inizializza lo stato del posto come libero.
+    public Posto() {
+        occupato = false; // All'inizio il posto è libero.
+    }
 
-	public Posto()
+    /**
+     * Verifica se il posto è libero.
+     * @return true se il posto è libero, false se è occupato.
+     */
+    public synchronized boolean libero() {
+        return (!occupato); // Restituisce lo stato di occupazione del posto.
+    }
 
-	{
-		occupato = false;
-	}
-
-	public synchronized boolean libero() {
-		return (!occupato);
-	}
-
-	public synchronized boolean occupa() {
-		if (occupato)
-			return false;
-		else
-			return occupato = true;
-	}
+    /**
+     * Occupa il posto se non è già occupato.
+     * @return true se l'occupazione è riuscita, false se il posto è già occupato.
+     */
+    public synchronized boolean occupa() {
+        if (occupato)
+            return false; // Se il posto è già occupato, non è possibile occuparlo.
+        else
+            return occupato = true; // Altrimenti, occupa il posto e ritorna true.
+    }
 }
