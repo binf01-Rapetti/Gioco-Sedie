725b9db92b01de7ed19965eea5f23b143acee3e5
binf01-Rapetti <105106982+binf01-Rapetti@users.noreply.github.com>
2025-01-16 13:26:14 +0100

    Update Display.java

--- Display.java
+++ Display.java
@@ -1,42 +1,45 @@
-package giocosedie;
 
-class Display extends Thread
+package GiocoSedieRapetti;
 
-{
-	private Posto sedie[];
-	private boolean endgame;
+class Display extends Thread {
+    private Posto sedie[];
+    private boolean endgame;
 
-	public Display(Posto sedie[]) {
+    // Costruttore che prende l'array delle sedie
+    public Display(Posto sedie[]) {
+        this.sedie = new Posto[sedie.length];
+        for (int s = 0; s < sedie.length; s++)
+            this.sedie[s] = sedie[s];
+    }
 
+    /**
+     * Metodo che visualizza lo stato attuale delle sedie e termina quando tutte le sedie sono occupate.
+     */
+    public void run() {
+        try {
+            while (!endgame) {
+                int count = 0; // Conta il numero di sedie occupate
 
-		this.sedie = new Posto[sedie.length];
+                // Ritardo casuale tra le visualizzazioni
+                sleep((int) (Math.random() * 250));
 
-		for (int s = 0; s < sedie.length; s++)
-			this.sedie[s] = sedie[s];
-	}
+                for (int i = 0; i < sedie.length; i++) {
+                    if (sedie[i].libero()) {
+                        System.out.print("0"); // La sedia è libera
+                    } else {
+                        count++;
+                        System.out.print("*"); // La sedia è occupata
+                    }
+                }
+                System.out.println();
 
-	public void run() {
-
-		try {
-			while (!endgame) {
-				int count = 0;
-
-				sleep((int) (Math.random() * 250));
-
-				for (int i = 0; i < sedie.length; i++) {
-				
-					if (sedie[i].libero())
-						System.out.print("0");
-					else {
-						count++;
-						System.out.print("*");
-					}
-				}
-				System.out.println();
-				endgame = (count == sedie.length);
-			}
-		} catch (InterruptedException e) {
-			throw new RuntimeException(e);
-		}
-	}
+                // Verifica se tutte le sedie sono occupate
+                endgame = (count == sedie.length);
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
+
+
