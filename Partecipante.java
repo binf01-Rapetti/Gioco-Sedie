28dab45390db8d58f85b8283fd7a72b3d365b929
binf01-Rapetti <105106982+binf01-Rapetti@users.noreply.github.com>
2025-01-16 13:26:55 +0100

    Update Partecipante.java

--- Partecipante.java
+++ Partecipante.java
@@ -1,33 +1,34 @@
-package giocosedie;
+package GiocoSedieRapetti;
 
 class Partecipante extends Thread
 
 {
-	Posto sedie[];
+Posto sedie[];
 
-	public Partecipante(Posto sedie[]) {
+public Partecipante(Posto sedie[]) {
 
-		this.sedie = sedie;
+this.sedie = sedie;
 
-	}
-
-	public void run() {
-
-		try {
-			sleep((int) (Math.random() * 1000));
-
-			for (int i = 0; i < sedie.length; i++) {
-				if (sedie[i].occupa()) {
-					System.out.println("Sono il Thread " + this.getName()
-							+ ". Sono riuscito a sedermi sul posto " + i);
-					return;
-				}
-			}
-			System.out.println("Sono il Thread " + this.getName()
-					+ ". Ho perso :((((");
-
-		} catch (InterruptedException e) {
-			throw new RuntimeException(e);
-		}
-	}
 }
+
+public void run() {
+
+try {
+sleep((int) (Math.random() * 1000));
+
+for (int i = 0; i < sedie.length; i++) {
+if (sedie[i].occupa()) {
+System.out.println("Sono il Thread " + this.getName()
++ ". Sono riuscito a sedermi sul posto " + i);
+return;
+}
+}
+System.out.println("Sono il Thread " + this.getName()
++ ". Ho perso :((((");
+
+} catch (InterruptedException e) {
+throw new RuntimeException(e);
+}
+}
+}
+
