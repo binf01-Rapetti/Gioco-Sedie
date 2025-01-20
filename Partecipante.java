0c22aff532d11081ee4c50b74967277f646d43c9
binf01-Rapetti <105106982+binf01-Rapetti@users.noreply.github.com>
2025-01-21 00:09:46 +0100

    Update Partecipante.java

--- Partecipante.java
+++ Partecipante.java
@@ -1,34 +1,45 @@
 package GiocoSedieRapetti;
 
-class Partecipante extends Thread
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
-{
-Posto sedie[];
+public class Partecipante extends Thread {
+    private Posto sedie[];
+    private static final String FILE_NAME = "Risultato.txt";
 
-public Partecipante(Posto sedie[]) {
+    public Partecipante(Posto sedie[]) {
+        this.sedie = sedie;
+    }
 
-this.sedie = sedie;
+    @Override
+    public void run() {
+        try {
+            sleep((int) (Math.random() * 1000)); // Attesa casuale prima di cercare un posto
 
+            for (int i = 0; i < sedie.length; i++) {
+                if (sedie[i].occupa()) {
+                    String risultato = "Partecipante " + this.getName() + " si è seduto sul posto " + i + "\n";
+                    salvaRisultato(risultato); // Salviamo il risultato nel file
+                    System.out.print(risultato);
+                    return;
+                }
+            }
+            String risultato = "Partecipante " + this.getName() + " ha perso\n";
+            salvaRisultato(risultato); // Salviamo il risultato nel file
+            System.out.print(risultato);
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // Metodo per salvare il risultato in un file
+    private synchronized void salvaRisultato(String risultato) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_NAME, true))) {
+            writer.write(risultato);
+        } catch (IOException e) {
+            throw new RuntimeException("Errore durante la scrittura del file: " + FILE_NAME, e);
+        }
+    }
 }
-
-public void run() {
-
-try {
-sleep((int) (Math.random() * 1000));
-
-for (int i = 0; i < sedie.length; i++) {
-if (sedie[i].occupa()) {
-System.out.println("Sono il Thread " + this.getName()
-+ ". Sono riuscito a sedermi sul posto " + i);
-return;
-}
-}
-System.out.println("Sono il Thread " + this.getName()
-+ ". Ho perso :((((");
-
-} catch (InterruptedException e) {
-throw new RuntimeException(e);
-}
-}
-}
-
